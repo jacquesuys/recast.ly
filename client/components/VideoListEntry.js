fd99ac5756edec65e2c5a9d82eaad266ee7dac31
jacquesuys <jaxuys@gmail.com>
2016-01-27 17:30:36 -0800

    rebind onclick

--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -3,12 +3,12 @@ const VideoListEntry = (props) => {
     props.onVideoSelect(props.video);
   };
 
-  return (<div className="video-list-entry" onClick={onVideoSelect}>
-    <div className="media-left media-middle">
+  return (<div className="video-list-entry">
+    <div className="media-left media-middle" onClick={onVideoSelect}>
       <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt={props.video.snippet.description} />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-title" onClick={onVideoSelect}>{props.video.snippet.title}</div>
       <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>)

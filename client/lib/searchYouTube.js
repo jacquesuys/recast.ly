b124535308d6673ee0fe18baf472d7a2bafe0a74
jacquesuys <jaxuys@gmail.com>
2016-01-27 18:46:06 -0800

    add ajax fn

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,18 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    method:'GET',
+    data: options,
+    type: 'video',
+    success: function(data) {
+      console.log(data);
+      callback(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  })
 };
 
 window.searchYouTube = searchYouTube;
